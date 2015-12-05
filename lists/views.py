95673ebfb84fe17a961cf3db53509caf7853e624
zhaoliang <yourzhaozhao@gmail.com>
2015-12-06 01:31:58 +0800

    Refactor new url

--- lists/views.py
+++ lists/views.py
@@ -3,12 +3,12 @@ from django.http import HttpResponse
 from lists.models import Item
 
 def home_page(request):
-    if request.method == 'POST':
-        next_item_text = request.POST['item_text']
-        Item.objects.create(text=next_item_text)
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
