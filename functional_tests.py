f88453c8744fbb53a207b7009c8adba43eb2e918
zhaoliang <yourzhaozhao@gmail.com>
2015-12-05 19:39:04 +0800

    Basic view now returns minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import unittest
 
-class NewVistorTest(unittest.TestCase):
+class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -10,13 +10,13 @@ class NewVistorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a coll new online to-do app. She goes
+        # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000/')
+        self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test')
+        self.fail('Finish the test!')
 
         # She is invited to enter a to-do item straight away
 
@@ -37,7 +37,8 @@ class NewVistorTest(unittest.TestCase):
 
         # She visits that URL - her to-do list is still there.
 
-        #  Satisfied, she goes back to sleep
+        # Satisfied, she goes back to sleep
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
