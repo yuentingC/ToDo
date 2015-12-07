b57c2ca1db9e91e755431c0127c7c9768350499a
zhaoliang <yourzhaozhao@gmail.com>
2015-12-07 23:21:57 +0800

    Add ExistingListItemForm

--- lists/tests/test_form.py
+++ lists/tests/test_form.py
@@ -1,6 +1,29 @@
 from django.test import TestCase
-from lists.forms import ItemForm, EMPTY_LIST_ERROR
+from lists.forms import (
+    ItemForm, EMPTY_LIST_ERROR,
+    DUPLICATE_ITEM_ERROR, ExistingListItemForm
+)
 from lists.models import List, Item
+
+class ExistingListItemFormTest(TestCase):
+
+    def test_form_renders_item_text_input(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_)
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+
+    def test_form_validation_for_blank_items(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [EMPTY_LIST_ERROR])
+
+    def test_form_validation_for_duplicate_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='no twins!')
+        form = ExistingListItemForm(for_list=list_, data={'text': 'no twins!'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
 
 
 class ItemFormTest(TestCase):
